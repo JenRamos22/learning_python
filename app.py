9f8501316871748110fe5bf7fe8e8e2910b43609
Jenny Ramos <jenraor@gmail.com>
2026-07-09 17:01:25 -0500

    Add calculate song duration function

---
+++ app.py
@@ -0,0 +1,23 @@
+
+jenn_playlist = [4.5, 2.8, 3.2, 5.0, 1.5]
+
+
+def calculate_song_duration(song):
+    total_songs = 0
+    for duration in song:
+        if duration >= 3.0:
+            total_songs += 1
+            
+            
+
+
+    print("The songs that last more than 3.0 minutes are:")
+    print(total_songs)
+
+
+calculate_song_duration(jenn_playlist)
+
+
+
+
+    
