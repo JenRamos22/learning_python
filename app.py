df6232bd29505505f897bb9382d103dfc8bfff8e
Jenny Ramos <jenraor@gmail.com>
2026-07-27 11:30:22 -0500

    organize folder by modules

--- app.py
+++ app.py
@@ -1,23 +1,5 @@
-
-luggage= [12, 25, 8, 23, 30, 15]
-
-def luggage_Weight(weigth_list):
-    heavy_suitcases = 0
-    standard_suitcases = 0
-
-    for weigth in weigth_list:
-        if weigth >= 23:
-            heavy_suitcases += 1
-
-        else: 
-            standard_suitcases += 1
-
-    print("heavy suitcases")
-    print(heavy_suitcases)
-    print("standard suitcases")
-    print(standard_suitcases)
-
-luggage_Weight(luggage)
+persona = {"nombre": "Juan", "edad": 25, "ciudad": "Madrid"}
+print(persona.keys())
 
 
 
